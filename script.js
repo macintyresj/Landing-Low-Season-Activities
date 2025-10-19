537808dfd49673dd04e80a6d737eda0fa91da8dc
Solange <72896376+macintyresj@users.noreply.github.com>
2025-10-19 19:53:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const games = [
+    {
+    title: "Aproximación",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Jeopardy",
+    image: "assets/img/jeopardy.png",
+    link: "https://macintyresj.github.io/FSO-Games-Trivia-Tablero-/",
+  },
+  {
+    title: "Preguntados",
+    image: "assets/img/preguntados.png",
+    link: "https://macintyresj.github.io/FSO-Games-Trivia/",
+  },
+  {
+    title: "First Shout",
+    image: "assets/img/firstshout.png",
+    link: "https://macintyresj.github.io/FSO-Games---First-Shout/",
+  }
+
+];
+// Crear bloques de color
+const bg = document.querySelector('.color-blocks');
+for (let i = 0; i < 8; i++) {
+  const div = document.createElement('div');
+  bg.appendChild(div);
+}
+
+const container = document.getElementById('games-container');
+
+games.forEach(game => {
+  const card = document.createElement('div');
+  card.className = 'game-card';
+  card.innerHTML = `
+    <img src="${game.image}" alt="${game.title}">
+    <h3>${game.title}</h3>
+  `;
+  card.addEventListener('click', () => window.open(game.link, '_blank'));
+  container.appendChild(card);
+});
