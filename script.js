667c49a5fc533533e8eb8bcd93ad4ba23c3e9d25
Solange <72896376+macintyresj@users.noreply.github.com>
2025-10-21 12:34:38 -0300

    Update game list and add new game entries

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const games = [
     {
     title: "Aproximation",
-    image: "",
-    link: "",
+    image:  "assets/img/Aproximation.png",
+    link: "https://people.ey.com/personal/agustin_villar_gds_ey_com/_layouts/15/onedrive.aspx?viewid=65e0aea4%2De09d%2D48f5%2Da83f%2Db5e77e7fea96&ga=1&id=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FAproximacion%2FAproximation%20%2D%20Instructions%2Epdf&parent=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FAproximacion",
   },
   {
     title: "Jeopardy",
@@ -24,10 +24,25 @@ const games = [
     image: "assets/img/firstshout.png",
     link: "https://macintyresj.github.io/FSO-Games---First-Shout/",
   },
+  {
+    title: "Act it out",
+    image: "assets/img/charades.png",
+    link: "https://people.ey.com/personal/agustin_villar_gds_ey_com/_layouts/15/onedrive.aspx?viewid=65e0aea4%2De09d%2D48f5%2Da83f%2Db5e77e7fea96&ga=1&id=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FDigalo%20con%20Mimica%2Finstructions%2Epdf&parent=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FDigalo%20con%20Mimica",
+  },
+ {
+    title: "The Word Game",
+    image: "assets/img/wordGame.png",
+    link: "https://people.ey.com/personal/agustin_villar_gds_ey_com/_layouts/15/onedrive.aspx?viewid=65e0aea4%2De09d%2D48f5%2Da83f%2Db5e77e7fea96&ga=1&id=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FEl%20juego%20de%20las%20palabras%2FThe%20word%20game%20%2D%20Instructions%2Epdf&parent=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FEl%20juego%20de%20las%20palabras",
+  },
   { 
     title: "I sit, I stand",
     image: "assets/img/GettyImages-553017003.jpg",
     link: "https://people.ey.com/:p:/r/personal/agustin_villar_gds_ey_com/_layouts/15/Doc.aspx?sourcedoc=%7BAE5E303D-23D6-4500-A296-B62CEC1F1232%7D&file=FSO%20Games%20-%20Support.pptx&action=edit&mobileredirect=true",
+  },
+      {
+    title: "T-Shirt Game",
+    image: "assets/img/TshirtGame.png",
+    link: "https://people.ey.com/personal/agustin_villar_gds_ey_com/_layouts/15/onedrive.aspx?viewid=65e0aea4%2De09d%2D48f5%2Da83f%2Db5e77e7fea96&ga=1&id=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FPoner%20camiseta%2FT%2Dshirt%20change%20istructions%2Epdf&parent=%2Fpersonal%2Fagustin%5Fvillar%5Fgds%5Fey%5Fcom%2FDocuments%2FFY%2026%2FADMIN%2FACTIVIDADES%2FPoner%20camiseta",
   },
 
 ];
@@ -50,6 +65,7 @@ games.forEach(game => {
   card.addEventListener('click', () => window.open(game.link, '_blank'));
   container.appendChild(card);
 });
+
 
 
 
